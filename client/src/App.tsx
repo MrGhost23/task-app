e43b284766d07a539bab92d8b35ba661dcfc3725
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-19 00:02:04 +0200

    initial client and server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,23 @@
-const App = () => {
-  return <div>App</div>;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import TaskDetail from "./pages/TaskDetail";
+import Navbar from "./components/Navbar";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/tasks/:id" element={<TaskDetail />} />
+      </Routes>
+    </Router>
+  );
 };
+
 export default App;
