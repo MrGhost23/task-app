602743a4fd39858a56f1f05051309532b3b89674
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-20 01:52:26 +0200

    made routes and CRUD controllers

--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, UpdateWriteOpResult } from 'mongoose';
 import { User } from 'src/Schemas/User.schema';
 
 @Injectable()
@@ -27,5 +27,46 @@ export class TasksService {
   async getAllTasksByUser(userId: string) {
     const tasks = await this.taskModel.find({ createdBy: userId }).exec();
     return tasks;
+  }
+
+  async updateTask(
+    taskId: string,
+    title: string,
+    description: string,
+    dueDate: Date,
+    category: string,
+  ): Promise<UpdateWriteOpResult> {
+    const updatedTask = await this.taskModel.updateOne(
+      { _id: taskId },
+      {
+        $set: {
+          title,
+          description,
+          dueDate,
+          category,
+        },
+      },
+    );
+    return updatedTask;
+  }
+
+  async deleteTask(taskId: string) {
+    const deletedTask = await this.taskModel.deleteOne({ _id: taskId });
+    return deletedTask;
+  }
+
+  async updateIsCompleted(
+    taskId: string,
+    isCompleted: boolean,
+  ): Promise<UpdateWriteOpResult> {
+    const updatedTask = await this.taskModel.updateOne(
+      { _id: taskId },
+      {
+        $set: {
+          isCompleted: isCompleted,
+        },
+      },
+    );
+    return updatedTask;
   }
 }
