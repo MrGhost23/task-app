f9d6d4c45cc78e2208a5551abb838a0c313333de
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-19 17:14:05 +0200

    added some ui with conditions and signup

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,4 +1,97 @@
-const Signup = () => {
-  return <div>Signup</div>;
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useDispatch } from "react-redux";
+import { login, register } from "@/store/slices/userAuthSlice";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(2).max(50),
+});
+
+const Signup: React.FC = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  interface RegisterPayload {
+    username: string;
+    password: string;
+  }
+
+  const dispatch = useDispatch();
+
+  function onSubmit(values: RegisterPayload) {
+    dispatch(
+      register({
+        username: values.username,
+        password: values.password,
+      })
+    );
+  }
+  return (
+    <div className="w-full flex justify-center items-center h-screen">
+      <div className="w-96">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg">Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="omarmohamed23"
+                      className="text-lg"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>Your LinkedIn Username</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg">Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="********"
+                      className="text-lg"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
+
 export default Signup;
