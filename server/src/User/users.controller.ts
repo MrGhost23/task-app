530adec5e27683b8435ecd2b8936b5520c528908
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-19 14:56:43 +0200

    added  enableCors and finished web scrapping linkedin profile

--- server/src/User/users.controller.ts
+++ server/src/User/users.controller.ts
@@ -10,6 +10,8 @@ import { UsersService } from './users.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from './Dtos/login-user.dto';
 import * as bcrypt from 'bcrypt';
+import { Builder, By } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
 
 @Controller('users')
 export class UserController {
@@ -23,24 +25,75 @@ export class UserController {
     return 'Hello There';
   }
 
-  // ...
-
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto) {
-    const { password, ...rest } = createUserDto;
+    const { username, password, ...rest } = createUserDto;
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+    const options = new Options().headless();
+    const driver = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
+    await driver.get(`https://www.linkedin.com/`);
+
+    const emailInput = await driver.findElement(
+      By.xpath("//input[@name='session_key' and @type='text']"),
+    );
+    await emailInput.sendKeys(process.env.LINKEDIN_EMAIL);
+
+    const passwordInput = await driver.findElement(
+      By.xpath("//input[@name='session_password' and @type='password']"),
+    );
+
+    await passwordInput.sendKeys(process.env.LINKEDIN_PASSWORD);
+
+    const signInButton = await driver.findElement(
+      By.xpath(
+        "//button[contains(@class, 'btn-md') and contains(@class, 'btn-primary') and contains(@class, 'flex-shrink-0') and contains(@class, 'cursor-pointer') and contains(@class, 'sign-in-form__submit-btn--full-width')]",
+      ),
+    );
+
+    await signInButton.click();
+
+    await driver.get(`https://www.linkedin.com/in/${username}/`);
+
+    const userImg = await driver.findElement(
+      By.xpath(
+        "//img[contains(@class, 'pv-top-card-profile-picture__image') and contains(@class, 'pv-top-card-profile-picture__image--show') and contains(@class, 'evi-image') and contains(@class, 'ember-view')]",
+      ),
+    );
+
+    const userImageSrc = await userImg.getAttribute('src');
+    const fullName = await driver.getTitle();
+
+    const names = fullName.split(' - ')[0].split(' ');
+    const firstTwoNames = names.slice(0, 2).join(' ');
+
+    console.log(userImageSrc);
+    console.log(firstTwoNames);
+
+    await driver.quit();
+
+    if (!firstTwoNames) {
+      throw new BadRequestException('Could not determine username');
+    }
+
     const user = await this.usersService.create({
       ...rest,
+      username: username || firstTwoNames,
       password: hashedPassword,
+      image: userImageSrc,
+      fullName: firstTwoNames,
     });
     const payload = { username: user.username, sub: user.id };
     return {
       userId: user.id,
       username: user.username,
       image: user.image,
+      fullName: user.fullName,
       access_token: this.jwtService.sign(payload),
     };
   }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { By, Builder } from 'selenium-webdriver';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const driver = await new Builder().forBrowser('chrome').build();
-  // await driver.get('https://www.linkedin.com/in/abdullah-nayer/');
+  app.enableCors();
 
-  // const userImage = await driver.findElement(
-  //   By.className('inline-block relative rounded-[50%] w-16 h-16'),
-  // );
-  // const userImageSrc = await userImage.getAttribute('src');
+  app.useGlobalPipes(new ValidationPipe());
 
-  // const fullName = await driver.getTitle();
-
-  // const names = fullName.split(' - ')[0].split(' ');
-  // const firstTwoNames = names.slice(0, 2).join(' ');
-
-  // console.log(userImageSrc);
-  // console.log(firstTwoNames);
-
-  // await driver.quit();
   await app.listen(5000);
 }
 bootstrap();
