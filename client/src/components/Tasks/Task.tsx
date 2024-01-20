583493b7a951b9803ea5128fb7f0924d4de613c3
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-20 15:30:36 +0200

    better ui and added toggleCompleted - without fetching

--- client/src/components/Tasks/Task.tsx
+++ client/src/components/Tasks/Task.tsx
@@ -22,25 +22,34 @@ const Task: React.FC<Props> = ({ task }) => {
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
+  const [taskIsCompleted, setTaskIsCompleted] = useState(task.isCompleted);
+
+  const toggleCompleted = () => {
+    setTaskIsCompleted((prevState) => !prevState);
+  };
+
   return (
     <>
       <div
         key={task.taskId}
-        className={`group relative h-full bg-white ${
-          task.isCompleted && "opacity-70 bg-green-200"
+        className={`group relative h-full  ${
+          taskIsCompleted ? "opacity-70 bg-green-200" : "bg-white"
         } rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-200 ease-in-out p-4`}
       >
         <h2
           className={`text-xl font-bold ${
-            task.isCompleted == true && "line-through"
+            taskIsCompleted == true && "line-through"
           }`}
         >
-          {task.isCompleted && (
+          {
             <IoMdCheckmarkCircleOutline
-              className="absolute top-2 right-2 text-green-500"
+              className={`absolute top-2 right-2 cursor-pointer ${
+                taskIsCompleted ? "text-green-500" : "text-gray-600"
+              }`}
               size={24}
+              onClick={toggleCompleted}
             />
-          )}
+          }
           {task.title}
         </h2>
         <p className="text-sm text-gray-500 capitalize">{task.category}</p>
@@ -50,13 +59,20 @@ const Task: React.FC<Props> = ({ task }) => {
             <MdOutlineDateRange className="shrink-0 text-lg" />
             <div className="flex flex-row flex-wrap items-center gap-1">
               {dateFormatter(task.dueDate)}
-              <span className="text-sm break-keep">({remainingDays}d left)</span>
+              <span className="text-sm break-keep">
+                ({remainingDays}d left)
+              </span>
             </div>
           </span>
           <div className="flex flex-row flex-wrap sm:flex-nowrap gap-2">
-            <Button className="bg-sky-500" onClick={() => setIsModalOpen(true)}>
-              Edit
-            </Button>
+            {!taskIsCompleted && (
+              <Button
+                className="bg-sky-500"
+                onClick={() => setIsModalOpen(true)}
+              >
+                Edit
+              </Button>
+            )}
             <Button
               className="bg-red-500"
               onClick={() => task.taskId && dispatch(deleteTask(task.taskId))}
