ae44c99ce175bcbff42037c8ffafc4f5f5031a1c
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-20 05:16:00 +0200

    added user bio

--- server/src/Schemas/User.schema.ts
+++ server/src/Schemas/User.schema.ts
@@ -14,6 +14,8 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
+  bio?: string;
+
   @Prop({
     default:
       'https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg',
