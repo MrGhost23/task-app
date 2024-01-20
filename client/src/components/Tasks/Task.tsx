06e6e13ccb46ce9f6af5a3bf57b2d2bc5d68f6ce
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-01-20 14:14:30 +0200

    added TaskForm and Modal

--- client/src/components/Tasks/Task.tsx
+++ client/src/components/Tasks/Task.tsx
@@ -6,6 +6,9 @@ import { MdOutlineDateRange } from "react-icons/md";
 import { Button } from "../ui/button";
 import { useAppDispatch } from "@/utils/hooks";
 import { deleteTask } from "@/store/slices/tasksSlice";
+import TaskForm from "@/components/TaskForm/TaskForm";
+import Modal from "@/components/ui/Modal";
+import { useState } from "react";
 
 interface Props {
   task: TaskType;
@@ -13,48 +16,60 @@ interface Props {
 
 const Task: React.FC<Props> = ({ task }) => {
   const today = new Date().toISOString().split("T")[0];
+  const remainingDays = calculateDaysDifference(today, task.dueDate);
 
   const dispatch = useAppDispatch();
 
-  const remainingDays = calculateDaysDifference(today, task.dueDate);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   return (
-    <div
-      key={task.taskId}
-      className={`group relative h-full bg-white ${
-        task.isCompleted && "opacity-70 bg-green-200"
-      } rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-200 ease-in-out p-4`}
-    >
-      <h2
-        className={`text-xl font-bold ${
-          task.isCompleted == true && "line-through"
-        }`}
+    <>
+      <div
+        key={task.taskId}
+        className={`group relative h-full bg-white ${
+          task.isCompleted && "opacity-70 bg-green-200"
+        } rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-200 ease-in-out p-4`}
       >
-        {task.isCompleted && (
-          <IoMdCheckmarkCircleOutline
-            className="absolute top-2 right-2 text-green-500"
-            size={24}
-          />
-        )}
-        {task.title}
-      </h2>
-      <p className="text-sm text-gray-500 capitalize">{task.category}</p>
-      <p className="mt-2 text-gray-700">{task.description}</p>
-      <div className="mt-4 flex justify-between items-center">
-        <span className="flex items-center gap-2">
-          <MdOutlineDateRange />
-          {dateFormatter(task.dueDate)} ({remainingDays}d left)
-        </span>
-        <div className="flex gap-2">
-          <Button className="bg-sky-500">Edit</Button>
-          <Button
-            className="bg-red-500"
-            onClick={() => task.taskId && dispatch(deleteTask(task.taskId))}
-          >
-            Delete
-          </Button>
+        <h2
+          className={`text-xl font-bold ${
+            task.isCompleted == true && "line-through"
+          }`}
+        >
+          {task.isCompleted && (
+            <IoMdCheckmarkCircleOutline
+              className="absolute top-2 right-2 text-green-500"
+              size={24}
+            />
+          )}
+          {task.title}
+        </h2>
+        <p className="text-sm text-gray-500 capitalize">{task.category}</p>
+        <p className="mt-2 text-gray-700">{task.description}</p>
+        <div className="mt-4 flex flex-col sm:flex-row flex-wrap justify-between sm:items-center gap-y-4">
+          <span className="flex flex-row items-center gap-2">
+            <MdOutlineDateRange className="shrink-0 text-lg" />
+            <div className="flex flex-row flex-wrap items-center gap-1">
+              {dateFormatter(task.dueDate)}
+              <span className="text-sm break-keep">({remainingDays}d left)</span>
+            </div>
+          </span>
+          <div className="flex flex-row flex-wrap sm:flex-nowrap gap-2">
+            <Button className="bg-sky-500" onClick={() => setIsModalOpen(true)}>
+              Edit
+            </Button>
+            <Button
+              className="bg-red-500"
+              onClick={() => task.taskId && dispatch(deleteTask(task.taskId))}
+            >
+              Delete
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+        <TaskForm setIsModalOpen={setIsModalOpen} taskToEdit={task} />
+      </Modal>
+    </>
   );
 };
 export default Task;
